1ade778a0f0750f487cf4363b44c307acb05baca
Adrian Garner <acgarner11@yahoo.com>
2021-06-23 14:03:33 -0400

    :rocket: Added All working links to the images to go to the form they are for.

--- frontpage.php
+++ frontpage.php
@@ -14,7 +14,7 @@
 get_header(); ?>
 
 
-    <div class="md:mx-40 mx-5 pt-16 grid grid-cols-12 gap-5">
+    <div class="md:mx-40 mx-5 pt-24 grid grid-cols-12 gap-5">
 <!--        <div class="md:pb-10 pb-5 col-span-2 ">-->
             <?php
             //start the repeater loop
@@ -26,7 +26,7 @@ get_header(); ?>
 
                     <div class="md:col-span-4 col-span-12 mb-6 md:ml-24">
                         <a href="<?php the_sub_field('link');?>"><img class="h-40" src="<?php the_sub_field('image');?>" alt="Icon"></a>
-                        <p><a href="<?php the_sub_field('image');?>" download><i class="fas fa-file-download"></i> Click To Download</a></p>
+
 
                 </div>
                 <?php
