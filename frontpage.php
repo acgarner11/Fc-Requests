c3fd0a20402826199d03cdf2de34212c1e8ef211
Adrian Garner <acgarner11@yahoo.com>
2021-06-23 11:01:40 -0400

    :tada: Initial Commit

---
+++ frontpage.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Template Name: Front Page
+ *
+ * The Frontpage of the Wordpack Theme
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package WordPress
+ * @subpackage Wordpack Theme
+ * @since 1.0.0
+ */
+
+get_header(); ?>
+
+
+    <div class="md:mx-40 mx-5 pt-5 grid grid-cols-12">
+        <div class="md:pb-10 pb-5 col-span-12 ">
+
+        </div>
+    </div>
+
+
+
+
+<?php
+get_footer();
