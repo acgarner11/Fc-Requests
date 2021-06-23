bdbdb6aa791491bb00c489b5c58bbc50da3c0af9
Adrian Garner <acgarner11@yahoo.com>
2021-06-23 11:49:33 -0400

    :construction: Added repeater to repeat blog images.

--- frontpage.php
+++ frontpage.php
@@ -16,7 +16,28 @@ get_header(); ?>
 
     <div class="md:mx-40 mx-5 pt-5 grid grid-cols-12">
         <div class="md:pb-10 pb-5 col-span-12 ">
+            <?php
+            //start the repeater loop
+            if (have_rows('repeater')):
+                //Card start
+                //Begin looping data
+                while (have_rows('repeater')) : the_row();
+                    ?>
 
+                    <div class="md:col-span-3 col-span-12 mb-10">
+                        <img src="<?php the_sub_field('image');?>" alt="Icon">
+                        <p><a href="<?php the_sub_field('image');?>" download><i class="fas fa-file-download"></i> Click To Download</a></p>
+                    </div>
+
+                <?php
+
+
+                endwhile;
+            else:
+                //no rows found
+            endif;
+
+            ?>
         </div>
     </div>
 
