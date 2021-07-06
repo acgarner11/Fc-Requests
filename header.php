7b7e136ba1ab554cf8af060453a8958fcc83e852
Adrian Garner <acgarner11@yahoo.com>
2021-07-06 12:55:52 -0400

    :construction:Removed button in nav bar.

--- header.php
+++ header.php
@@ -82,12 +82,13 @@
                     <ul class="list-reset lg:flex justify-end flex-1 items-center">
 
                     </ul>
-                    <button
-                            id="navAction"
-                            class="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full mt-4 lg:mt-0 py-4 px-8 shadow opacity-75 focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out"
-                    >
-                        Action
-                    </button>
+<!--                    If you need a button add this-->
+<!--                    <button-->
+<!--                            id="navAction"-->
+<!--                            class="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full mt-4 lg:mt-0 py-4 px-8 shadow opacity-75 focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out"-->
+<!--                    >-->
+<!--                        Action-->
+<!--                    </button>-->
 
 
                 </div>
